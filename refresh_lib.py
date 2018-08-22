236982d6d5445faa32f75c0541fb2b044ee9c8c6
dan <daniel.j.menz@gmail.com>
2018-08-22 22:51:52 +1000

    added date selections for youtube api so all youtube links are foudn'

--- refresh_lib.py
+++ refresh_lib.py
@@ -33,15 +33,17 @@ def refresh_link_database():
 		else:
 			print ('invalid platform in database')
 
-	print('---Soundcloud')	
-	for artist in soundcloud_artists:
-		sc_refresh_link_database_for_artist(artist)
-		print('Completed: ' + artist)
-
 	print('---Youtube')
 	for artist in youtube_artists:
+		print('Refreshing: ' + artist)
 		yt_refresh_link_database_for_artist(artist)
-		print('Have not quite completed: ' + artist)
+		print('Completed: ' + artist + '\n')
+
+	print('---Soundcloud')	
+	for artist in soundcloud_artists:
+		print('Refreshing: ' + artist)
+		sc_refresh_link_database_for_artist(artist)
+		print('Completed: ' + artist + '\n')
 
 	return
 
@@ -90,7 +92,7 @@ def sc_refresh_link_database_for_artist(artist_to_dl):
 	for link in soup.find_all('a'):
 		if str(link).find('/' + artist_to_dl + '/') > 0:
 			if not (str(link.get('href')).endswith('/comments')):
-				print(link.get('href'))
+				#print(link.get('href'))
 				if (str(link.get('href')).split('/')[2]) not in non_mixes:
 					link_frags.add(link.get('href'))
 
@@ -102,12 +104,13 @@ def sc_refresh_link_database_for_artist(artist_to_dl):
 			#print(full_link)
 			links_full.append([full_link,link.split('/')[2]])
 
-	#print(len(links_full))
+	print('Number of links: ' + str(len(links_full)))
 	
 	dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
 	table = dynamodb.Table('music_url_archive')
 
 	print('--- refreshing links in db')
+	counter = 0
 	for url in links_full:
 		try:
 			table.put_item(
@@ -122,9 +125,11 @@ def sc_refresh_link_database_for_artist(artist_to_dl):
 				},
 				ConditionExpression='attribute_not_exists(url_link)'
 			)
+			counter += 1
 		except Exception as e:
-			print('already in database');
+			#print('already in database');
 			continue
+	print('links added: ' + str(counter))
 	return
 
 def yt_refresh_link_database_for_artist_slow(artist_to_dl):
@@ -156,7 +161,7 @@ def yt_refresh_link_database_for_artist_slow(artist_to_dl):
 
 	for url in links_full:
 		try:
-			print(url[0])
+			#print(url[0])
 			table.put_item(
 				Item={
 					'url_link': url[0],
@@ -170,7 +175,7 @@ def yt_refresh_link_database_for_artist_slow(artist_to_dl):
 				ConditionExpression='attribute_not_exists(url_link)'
 			)
 		except Exception as e:
-			print('already in database')
+			#print('already in database')
 			continue
 
 	return
@@ -204,32 +209,49 @@ def yt_refresh_link_database_for_artist(artist_to_dl):
 	base_video_url = 'https://www.youtube.com/watch?v='
 	base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
-	first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=50'.format(api_key, channel_id)
+
+	# Searches all years between 2009 - 2021
+	years_RFC3339_pairs = []
+	for x in range(2009,2021):
+		start_year = (str(x)+'-01-01T00:00:00Z')
+		finish_year = (str(x+1)+'-01-01T00:00:00Z')
+		years_RFC3339_pairs.append([start_year,finish_year])
+
+	urls_by_date = []
+	for row in years_RFC3339_pairs:
+		search_start_date = row[0]
+		search_finish_date = row[1]
+		url_one_year = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=50&publishedAfter={}&publishedBefore={}'.format(api_key, channel_id,search_start_date,search_finish_date)
+		urls_by_date.append(url_one_year)
 
 	links_full = []
-	url = first_url
 
-	while True:
-		inp = urllib.request.urlopen(url)
-		resp = json.load(inp)
+	for url in urls_by_date:
+		first_url = url
 
-		for i in resp['items']:
-			if i['id']['kind'] == "youtube#video":
-				temp_url = (base_video_url + i['id']['videoId'])
-				links_full.append([temp_url, i['snippet']['title']])
-		try:
-			next_page_token = resp['nextPageToken']
-			url = first_url + '&pageToken={}'.format(next_page_token)
-		except:
+		while True:
+			inp = urllib.request.urlopen(url)
+			resp = json.load(inp)
 
-			break
+			for i in resp['items']:
+				if i['id']['kind'] == "youtube#video":
+					temp_url = (base_video_url + i['id']['videoId'])
+					links_full.append([temp_url, i['snippet']['title']])
+			try:
+				next_page_token = resp['nextPageToken']
+				url = first_url + '&pageToken={}'.format(next_page_token)
+			except:
+
+				break
 
 	dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
 	table = dynamodb.Table('music_url_archive')
 
+	print('Number of links: ' + str(len(links_full)))
+	counter = 0
 	for url in links_full:
 		try:
-			print(url[0])
+			#print(url[0])
 			table.put_item(
 				Item={
 					'url_link': url[0],
@@ -242,8 +264,30 @@ def yt_refresh_link_database_for_artist(artist_to_dl):
 				},
 				ConditionExpression='attribute_not_exists(url_link)'
 			)
+			counter += 1
 		except Exception as e:
-			print('already in database')
+			#print('already in database')
 			continue	
-
+	print('links added: ' + str(counter))
 	return
+
+# This is for testing purposes - just refreshes the 1st youtube artist only
+def main():
+	print('Testing')
+	artist_list = get_artists_to_download()
+
+	youtube_artists = []
+	
+	for artist_row in artist_list:
+		if (artist_row['platform'] == 'youtube'):
+			youtube_artists.append(artist_row['artist'])
+		else:
+			print ('invalid platform in database')
+
+	print('---Youtube')
+	print(youtube_artists[4])
+	yt_refresh_link_database_for_artist(youtube_artists[4])
+	
+
+if __name__ == "__main__":
+	main()
