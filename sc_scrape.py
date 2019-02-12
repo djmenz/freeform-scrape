9d7fdc1a8b74df7ed7b3e0d23c9024ba8f271b2d
dan <daniel.j.menz@gmail.com>
2019-02-12 21:39:30 +1100

    added filename to classifier

--- sc_scrape.py
+++ sc_scrape.py
@@ -113,7 +113,9 @@ def download_one_track(url_row):
 				info_dict = ydl.extract_info(url, download=False)
 				filename = ydl.prepare_filename(info_dict)
 				name_only = filename[len(base_dir):].rsplit('.',1)[0]
-				ext = filename[len(base_dir):].rsplit('.')[-1:]
+				ext = filename
+				print('ExTis')
+				print(ext)
 				print('FILE IS:' + name_only)
 				result = ydl.download([url])
 				print("downloaded:" + url)
